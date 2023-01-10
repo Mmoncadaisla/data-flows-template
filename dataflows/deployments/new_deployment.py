831f1a22879875f0ec220259b36af9dde9a86308
Mmoncadaisla <mmoncada@cartodb.com>
2023-01-10 16:00:38 +0100

    Add new flow

---
+++ dataflows/deployments/new_deployment.py
@@ -0,0 +1,21 @@
+from prefect.deployments import Deployment
+from prefect.blocks.core import Block
+from prefect.orion.schemas.schedules import CronSchedule
+from dataflows.flows.hello_flow import hello_flow
+
+storage = Block.load("gcs/default")
+infrastructure = Block.load("cloud-run-job/default")
+
+
+def deploy_hello_flow():
+    deployment = Deployment.build_from_flow(
+        flow=hello_flow,
+        name="hello_flow_deployment",
+        work_queue_name="default",
+        storage=storage,
+        path="hello_flow",
+        tags=["staging"],
+        infrastructure=infrastructure,
+        schedule=CronSchedule(cron="0 12 1 * *", timezone="UTC"),
+    )
+    deployment.apply()
